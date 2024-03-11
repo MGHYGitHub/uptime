cca27f0a1aaebc12a32c7789fcdcc54445dbbc1c
MGHY <2970659912@qq.com>
2024-03-12 00:16:10 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,42 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '橙子🍊 站点监测',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'm796548491-cce3e47d7432fd4ce9ed262f',
+    'm796548495-b790642c0da2f32cf8c57b8d',
+    'm796548774-1f29c9d72c6a332ba05b0c52',
+    'm796548656-fbd0e64e72fb663ae856248a',
+    'm796548523-626cab7f26af9d0b2a278d24',
+    'm796548584-295e8b6e4d928985ab89ab0c',
+    'm796548576-125571e5e1ee87b4ccdfbdda',
+    'm796548563-57defdd6889c78b77155b88c',
+    'm796548629-f258edb5d9e82c6931d54ce7',
+
+  ],
+
+  // 日志天数
+  CountDays: 60,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Homepage',
+      url: 'https://zy.mghy.top/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/MGHYGitHub'
+    },
+    {
+      text: 'Blog',
+      url: 'https://mghy.top/'
+    },
+  ],
+};
